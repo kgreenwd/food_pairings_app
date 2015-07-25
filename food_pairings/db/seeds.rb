38980623c9962da62db12440eef074f84b665696
kgreenwd <2013kgreenwood@gmail.com>
2015-07-25 20:50:08 +0200

    generated Dish and Drink models, migrated database, added seeds

--- food_pairings/db/seeds.rb
+++ food_pairings/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dish.delete_all
+Drink.delete_all
+
+puts "Creating dishes and drinks..."
+
+Dish.create name: "Flamin Hot Cheetos", description: "so good in the moment but so bad in the bathroom"
+Dish.create name: "Mac n Cheese", description: "cheese n carbs"
+Dish.create name: "Ramen", description: "dude, drop an egg in the boiling water, you won't regret it i promise"
+
+Drink.create name: "Moscato", description: "preferably the nicki minaj brand"
+Drink.create name: "Jager Bombs", description: "insert attack on titan joke lolol"
+Drink.create name: "Natty Light", description: "moldy water"
